1f8ecc7533a9f7c8ac969bf6d20d11beb9d7388e
Berside <144826358+Berside@users.noreply.github.com>
2023-10-06 18:05:03 +0300

    homework v2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,8 @@
+# Импорт модулей
 import json
 import xml.etree.ElementTree as ET
 
-
+# Классы
 class Building:
     year = None #int
     city = None #string
@@ -56,18 +57,20 @@ class House(Building):
         print("Family:" , self.family, " free:", self.free)
 
 
-
+#Класс ошибок
 class MYException(Exception):
     def __init__(self, message="Invalid input"):
         self.message = message
         super().__init__(self.message)
 
 
+#Функция чтения JSON файла
 def readJSON(filename):
     with open(filename, 'r') as file:
         return json.load(file)
 
 
+#Функция записи JSON файла
 def WriteJSON(data,filename):
     data = json.dumps(data)
     data = json.loads(str(data))
@@ -75,9 +78,7 @@ def WriteJSON(data,filename):
         json.dump(data,file,indent=4)
 
 
-bd = {
-    "Buildings" : [] # в массив добавляем обьекты
-}
+#Создание обьектов
 
 mag = shop(True, "Eat", 21, "Moscow")
 
@@ -87,7 +88,13 @@ house1 = House(True, "Poverinov", 2, "Moscow")
 
 house2 = House(False, "None", 5, "None")
 
+#Запись обьектов в JSON файл
+
 listc = [mag,mag2,house1,house2]
+
+bd = {
+    "Buildings" : [] # в массив добавляем обьекты
+}
 
 for i in range (len(listc)):
     bd['Buildings'].append(listc[i].__dict__)
@@ -95,12 +102,15 @@ WriteJSON(bd,'DB.json')
 
 #print(readJSON('DB.json'))
 
-ndata =  readJSON('DB.json')
+#Чтение JSON файла
 
-#g = Shop(ndata['Buildings'][0]['works'], ndata['Buildings'][0]['products'],ndata['Buildings'][0]['year'],ndata['Buildings'][0]['city'])
+ndata = readJSON('DB.json')
 
-#g.getInf()
+g = shop(ndata['Buildings'][0]['works'], ndata['Buildings'][0]['products'],ndata['Buildings'][0]['year'],ndata['Buildings'][0]['city'])
 
+g.getInf()
+
+#Чтение XML файла
 
 lista = [mag, mag2]
 
@@ -108,36 +118,39 @@ listb = [house1,house2]
 
 tree = ET.parse('line.xml')
 root = tree.getroot()
-works = root.find('Works').text
-products = root.find('Products').text
-year = root.find('Year').text
-city = root.find('City').text
-babs =  shop(works,products,year,city)
-babs.getInf()
-# buildings = ET.Element('Buildings')
-#
-# for i in range (len(lista)):
-#     id = ET.SubElement(buildings, 'Shop')
-#     idw = ET.SubElement(id, 'Works')
-#     idp = ET.SubElement(id, 'Products')
-#     idy = ET.SubElement(id, 'Year')
-#     idc = ET.SubElement(id, 'City')
-#     idw.text = str(lista[i].works)
-#     idp.text = str(lista[i].products)
-#     idy.text = str(lista[i].year)
-#     idc.text = str(lista[i].city)
-#
-# for i in range (len(listb)):
-#     id = ET.SubElement(buildings, 'House')
-#     idw = ET.SubElement(id, 'free')
-#     idp = ET.SubElement(id, 'family')
-#     idy = ET.SubElement(id, 'Year')
-#     idc = ET.SubElement(id, 'City')
-#     idw.text = str(listb[i].free)
-#     idp.text = str(listb[i].family)
-#     idy.text = str(listb[i].year)
-#     idc.text = str(listb[i].city)
-#
-# data = ET.tostring(buildings, encoding='unicode')
-# file = open('line.xml', 'w')
-# file.write(data)
+for mag_element in root.findall('Shop'):
+    works = bool(mag_element.find('Works').text)
+    products = mag_element.find('Products').text
+    year = int(mag_element.find('Year').text)
+    city = mag_element.find('City').text
+    a = shop(works,products,year,city)
+    a.getInf()
+
+#Создание древовидной структуры XML и запись
+
+buildings = ET.Element('Buildings')
+for i in range (len(lista)):
+    id = ET.SubElement(buildings, 'Shop')
+    idw = ET.SubElement(id, 'Works')
+    idp = ET.SubElement(id, 'Products')
+    idy = ET.SubElement(id, 'Year')
+    idc = ET.SubElement(id, 'City')
+    idw.text = str(lista[i].works)
+    idp.text = str(lista[i].products)
+    idy.text = str(lista[i].year)
+    idc.text = str(lista[i].city)
+
+for i in range (len(listb)):
+    id = ET.SubElement(buildings, 'House')
+    idw = ET.SubElement(id, 'free')
+    idp = ET.SubElement(id, 'family')
+    idy = ET.SubElement(id, 'Year')
+    idc = ET.SubElement(id, 'City')
+    idw.text = str(listb[i].free)
+    idp.text = str(listb[i].family)
+    idy.text = str(listb[i].year)
+    idc.text = str(listb[i].city)
+
+data = ET.tostring(buildings, encoding='unicode')
+file = open('line.xml', 'w')
+file.write(data)
